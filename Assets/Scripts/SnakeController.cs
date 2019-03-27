4a6a208e5968422aca3a3e876898a3f3617d78e1
Axel <oberonx10@gmail.com>
2019-03-27 03:51:00 +0000

    Created Initial Snake Scripts

---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_snakeHead;
+    [SerializeField]
+    private Transform m_snakeBody;
+    [SerializeField]
+    private GameObject m_snakeTrail;
+    protected GameObject SnakeTrail { get { return m_snakeTrail; } }
+}
